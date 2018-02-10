a7bc7d1f9766b3baaefe31207bf99edcdff3b9e9
Tom Corcoran <tnscorcoran@gmail.com>
2018-02-11 09:32:04 +1100

    next commit

---
+++ src/main/java/app/GenericResult.java
@@ -0,0 +1,21 @@
+package app;
+
+public class GenericResult {
+	private String summary;
+	
+	public GenericResult() {
+		super();
+	}
+
+	public GenericResult(String summary) {
+		super();
+		this.summary = summary;
+		
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	
+}
