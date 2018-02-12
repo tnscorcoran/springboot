1ccace3c9a8eb7a2a9f38f5561ef08bbafe950e8
Tom Corcoran <tnscorcoran@gmail.com>
2018-02-12 21:29:46 +1100

    commit

--- src/main/java/app/GreetingController.java
+++ src/main/java/app/GreetingController.java
@@ -1,6 +1,14 @@
 package app;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,4 +24,74 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+    
+    @RequestMapping("/greetingpv")
+    public Greeting greetingpv(@RequestParam(value="name", defaultValue="World") String name) {
+        
+	    	System.out.println("*********** PV 1 ************");
+	    	
+	    	readWriteFile();
+	 
+	    	System.out.println("*********** PV 2 ************");
+    		
+    	
+    		return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+    
+    //https://www.caveofprogramming.com/java/java-file-reading-and-writing-files-in-java.html
+    public void readWriteFile () {
+
+        // This will reference one line at a time
+        String line = null;
+
+        
+        String lyrics = "I, I will be king And you, you will be queen Though nothing, will drive them away We can beat them, just for one day We can be heroes, just for one day And you, you can be mean And I, I'll drink all the time 'Cause we're lovers, and that is a fact Yes we're lovers, and that is that";
+        
+        
+        String fileNameAndPath =  System.getenv("OPENSHIFT_DATA_DIR") + "/temp.txt";
+        File f = new File(fileNameAndPath);
+
+        
+        System.out.println("f::AbsolutePath:"+ f.getAbsolutePath());
+        System.out.println("OPENSHIFT_DATA_DIR:"+System.getenv("OPENSHIFT_DATA_DIR"));
+
+        try {
+            
+        	// Write to file
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileNameAndPath));
+            writer.write(lyrics);             
+            writer.close();
+
+        	
+        	// FileReader reads text files in the default encoding.
+            FileReader fileReader = 
+                new FileReader(fileNameAndPath);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = 
+                new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                		fileNameAndPath + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileNameAndPath + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+    }    
+    
+
 }
